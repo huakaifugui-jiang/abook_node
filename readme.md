bba4e912c87abf81099d82c3ddb7a0ed6f71b590
wulongjiang <562445380@qq.com>
2023-06-04 22:41:43 +0800

    feat: 开始web服务器的构建

--- readme.md
+++ readme.md
@@ -7,3 +7,5 @@
 3. eslint 设置代码的规范 增加代码的健壮性
 4. husky 和 lint-staged husky 是 git hook 钩子 可以在提交代码之前进行操作 比如进行 eslint lint-staged 是对暂存代码进行 eslint
 5. 实时更新代码 目的是 在文件变化时可以实时的更新代码 此时需要用到三个包 rimraf（用于跨平台兼容 删除目录 dist） concurrently （用于可以同时执行几个命令 比如 tsc -w 和 nodemon） nodemon（用于实时监听文件的变化并且更新） 项目运行： npm run start 就可以实时运行项目了
+
+## 构建 web 服务器
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('server running');
+import http from 'http';
+
+const app = http.createServer((req, res) => {
+  console.log(req.method);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://127.0.0.1:${port}`);
+});

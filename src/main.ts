2a747c4e77c648ca1bda4525c19af8d5812004db
wulongjiang <562445380@qq.com>
2023-06-07 22:42:39 +0800

    feat: 网络编程学习

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,8 @@
-import net from 'net';
+import http from 'http';
 
-const server = net.createServer(socket => {
-  console.log('Client connected');
-
-  socket.on('data', data => {
-    console.log('Received data');
-
-    // 构造HTTP响应
-    const responseBody = 'Hello, World!';
-    const response = `HTTP/1.1 200 OK\r\n\Content-Length: ${responseBody.length}\r\n\r\n${responseBody}`;
-
-    // 发送HTTP响应
-    socket.write(response);
-  });
+const server = http.createServer((req, res) => {
+  res.write('hello world');
+  res.end();
 });
 
-server.on('connection', socket => {
-  console.log('connetction');
-  socket.on('data', data => {
-    console.log('conection data',data.toString());
-  });
-});
-
-server.listen(8124, () => {
-  console.log('TCP server running on port 8124');
-});
-
+server.listen(8124);
